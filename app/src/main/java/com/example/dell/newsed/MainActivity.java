3aa5cf7faf3a582159afb56681856125b762e964
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-09-27 03:45:54 +0530

    Implemented Adapter, new layout for adapter

--- app/src/main/java/com/example/dell/newsed/MainActivity.java
+++ app/src/main/java/com/example/dell/newsed/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.dell.newsed;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Adapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
     private static final String REQUESTED_URL = "https://content.guardianapis.com/search?q=Technology&format=json&order-by=newest&page-size=20&from-date=2018-09-01&show-fields=headline,thumbnail,short-url&show-tags=contributor,publication&api-key=751d026c-5315-4412-824f-90852ee18451";
+    private static final int NEWS_LOADER_ID = 1;
+    private Adapter adapter;
+    private ProgressBar progressBar;
+    private TextView emptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView newsListView =(ListView) findViewById(R.id.list);
+        emptyStateTextView = (TextView) findViewById(R.id.empty_view);
     }
 }
---
+++ app/src/main/java/com/example/dell/newsed/NewsAdapter.java
@@ -0,0 +1,44 @@
+package com.example.dell.newsed;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends ArrayAdapter<NewsFeatures>{
+
+        ArrayList<NewsFeatures> list;
+        NewsAdapter(@NonNull Context context,ArrayList<NewsFeatures> list){
+            super(context,R.layout.activity_main,R.id.list,list);
+            this.list = list;
+        }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = convertView;
+        if(view==null){
+            view = LayoutInflater.from(getContext()).inflate(R.layout.news,parent,false);
+
+        }
+        NewsFeatures newsFeatures  = list.get(position);
+        ImageView image = view.findViewById(R.id.thumbnail);
+        TextView headline = view.findViewById(R.id.headline);
+        TextView author = view.findViewById(R.id.author);
+        TextView date = view.findViewById(R.id.date);
+
+        headline.setText(newsFeatures.getHeadline());
+        author.setText(newsFeatures.getAuthor());
+        date.setText(newsFeatures.getPublicationDate());
+        image.setImageBitmap(newsFeatures.getThumbnail());
+           return view;
+        }
+}
