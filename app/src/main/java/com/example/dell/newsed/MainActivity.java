75cf1f330b8d417b534e22f7c2f064776b182c81
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-09-27 15:39:27 +0530

    Implemented code for empty state. Mail intent and Networks settings intent

--- app/src/main/java/com/example/dell/newsed/MainActivity.java
+++ app/src/main/java/com/example/dell/newsed/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.dell.newsed;
+
 import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
@@ -6,12 +7,16 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsFeatures>> {
@@ -21,24 +26,42 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private NewsAdapter newsAdapter;
     private ProgressBar progressBar;
     private TextView emptyStateTextView;
+    private ImageView emptyImageView;
+    private Button emptyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView newsListView =(ListView) findViewById(R.id.list);
         emptyStateTextView = (TextView) findViewById(R.id.empty_view);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         newsListView.setEmptyView(emptyStateTextView);
+        emptyImageView = findViewById(R.id.duck);
+        newsListView.setEmptyView(emptyImageView);
+        emptyButton = findViewById(R.id.empty_button);
+        emptyButton.setVisibility(View.GONE);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
         //adapter code
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         if(networkInfo==null|| !networkInfo.isConnected()){
             progressBar.setVisibility(View.GONE);
             emptyStateTextView.setText(R.string.no_internet);
+            emptyImageView.setImageResource(R.drawable.sad_duck);
+            emptyButton.setVisibility(View.VISIBLE);
+            emptyButton.setText("Open Internet Settings");
+            emptyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent settingsIntent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                    startActivity(settingsIntent);
+                }
+            });
         }
         else {
             LoaderManager loaderManager  = getLoaderManager();
             loaderManager.initLoader(NEWS_LOADER_ID,null,this);
+
         }
         newsAdapter = new NewsAdapter(this, new ArrayList<NewsFeatures>());
         newsListView.setAdapter(newsAdapter);
@@ -58,11 +81,26 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
     @Override
     public void onLoadFinished(Loader<List<NewsFeatures>> loader, List<NewsFeatures> data) {
-         progressBar.setVisibility(View.GONE);
-         emptyStateTextView.setText(R.string.no_news);
-         newsAdapter.clear();
+        progressBar.setVisibility(View.GONE);
          if(data!=null&&!data.isEmpty()){
              newsAdapter.addAll(data);
+         }
+         else{
+             emptyButton.setVisibility(View.VISIBLE);
+             emptyButton.setText("Mail Developer");
+             emptyStateTextView.setText(R.string.no_news);
+             emptyImageView.setImageResource(R.drawable.sad_duck);
+             newsAdapter.clear();
+             emptyButton.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                             "mailto","srivastava.shivanshu4@gmail.com", null));
+                     emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Newsed app is not working");
+                     emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+                     startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                 }
+             });
          }
     }
     @Override
