31445cd8f82cfabefcf20c82e6e13443950e1c96
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-10-07 15:26:54 +0530

    preferences screen added

--- app/src/main/java/com/example/dell/newsed/SettingsActivity.java
+++ app/src/main/java/com/example/dell/newsed/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.newsed;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -12,5 +13,13 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
     public static class NewsPreferenceFragment extends PreferenceFragment {
-    }
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+        }
+}
+
+
+
 }
